fc9b0bf1553a938d7e8db8d0b0b25db08676d208
Jason Gritman <jasongritman@gmail.com>
2012-06-19 17:31:48 -0700

    Initial commit.

---
+++ ContextParamGrailsPlugin.groovy
@@ -0,0 +1,79 @@
+import com.netflix.grails.contextParam.ContextParam
+import org.codehaus.groovy.grails.commons.ControllerArtefactHandler
+import org.codehaus.groovy.grails.commons.GrailsControllerClass
+import org.springframework.web.context.request.RequestContextHolder
+
+class ContextParamGrailsPlugin {
+
+    def observe = ["controllers"]
+
+    // the plugin version
+    def version = "1.0"
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "1.3.7 > *"
+    def loadAfter = ['controllers']
+
+    def author = "Jason Gritman"
+    def authorEmail = "jgritman@netflix.com"
+    def title = "Plugin summary/headline"
+    def description = '''\\
+Brief description of the plugin.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/context-param"
+
+    def doWithDynamicMethods = { ctx ->
+        Map<String, List<String>> controllerNamesToContextParams = [:]
+        for (controllerClass in application.controllerClasses) {
+            Collection<String> contextParams = findContextParams(controllerClass)
+            replaceRedirectMethod(controllerClass, contextParams)
+            controllerNamesToContextParams[(controllerClass.logicalPropertyName)] = contextParams
+        }
+        application.metaClass.getControllerNamesToContextParams {
+            controllerNamesToContextParams
+        }
+    }
+
+    def onChange = { event -> 
+        if(event.source instanceof Class &&
+                application.isArtefactOfType(ControllerArtefactHandler.TYPE, event.source)) {
+            GrailsControllerClass controllerClass = application.getArtefact(ControllerArtefactHandler.TYPE, 
+                    event.source.name)
+            Collection<String> contextParams = findContextParams(controllerClass)
+            replaceRedirectMethod(controllerClass, contextParams)
+            application.controllerNamesToContextParams[(controllerClass.logicalPropertyName)] = contextParams
+        }
+    }
+
+    private void replaceRedirectMethod(GrailsControllerClass controllerClass, Collection<String> contextParams) {
+        def oldRedirect = controllerClass.metaClass.pickMethod("redirect", [Map] as Class[])
+        controllerClass.metaClass.redirect = { Map args ->
+            appendContextParams(contextParams, args)
+            oldRedirect.invoke(delegate, args)
+        }
+        def oldChain = controllerClass.metaClass.pickMethod("chain", [Map] as Class[])
+        controllerClass.metaClass.chain = { Map args ->
+            appendContextParams(contextParams, args)
+            oldChain.invoke(delegate, args)
+        }
+    }
+
+    private void appendContextParams(Collection<String> contextParams, Map arguments) {
+        if (arguments && contextParams ) {
+            // If redirect was called without a params map object then make an empty params map.
+            Map params = arguments.params ?: [:]
+            def request = RequestContextHolder.currentRequestAttributes()
+            contextParams.each { String contextParam ->
+                if (!params[(contextParam)]) { 
+                    params[(contextParam)] = request.params[(contextParam)]
+                }
+            }
+            arguments.params = params
+        }
+    }
+
+    private Collection<String> findContextParams(GrailsControllerClass controllerClass) {
+        controllerClass.clazz.declaredAnnotations.findAll { it instanceof ContextParam }.collect { it.value() }
+    }
+}
---
+++ src/groovy/com/netflix/grails/contextParam/ContextParam.groovy
@@ -0,0 +1,12 @@
+package com.netflix.grails.contextParam
+
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface ContextParam {
+    String value()
+}
